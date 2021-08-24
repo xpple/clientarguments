dc6eab86d9520ea1fea7b0887e96b8962c7b127d
xpple <fred@xpple.dev>
2021-08-24 22:29:06 +0200

    Fix broken vanilla code

--- src/main/java/dev/xpple/clientarguments/arguments/CScoreboardCriterionArgumentType.java
+++ src/main/java/dev/xpple/clientarguments/arguments/CScoreboardCriterionArgumentType.java
@@ -1,6 +1,5 @@
 package dev.xpple.clientarguments.arguments;
 
-import com.google.common.collect.Lists;
 import com.mojang.brigadier.StringReader;
 import com.mojang.brigadier.arguments.ArgumentType;
 import com.mojang.brigadier.context.CommandContext;
@@ -8,25 +7,18 @@ import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.mojang.brigadier.exceptions.DynamicCommandExceptionType;
 import com.mojang.brigadier.suggestion.Suggestions;
 import com.mojang.brigadier.suggestion.SuggestionsBuilder;
+import net.fabricmc.fabric.api.client.command.v1.FabricClientCommandSource;
 import net.minecraft.command.CommandSource;
 import net.minecraft.scoreboard.ScoreboardCriterion;
-import net.fabricmc.fabric.api.client.command.v1.FabricClientCommandSource;
-import net.minecraft.stat.Stat;
-import net.minecraft.stat.StatType;
 import net.minecraft.text.TranslatableText;
-import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
-import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 public class CScoreboardCriterionArgumentType implements ArgumentType<ScoreboardCriterion> {
 
-	private static final Collection<String> EXAMPLES = Arrays.asList("foo", "foo.bar.baz", "minecraft:foo");
+	private static final Collection<String> EXAMPLES = Arrays.asList("trigger", "playerKillCount", "food");
 	public static final DynamicCommandExceptionType INVALID_CRITERION_EXCEPTION = new DynamicCommandExceptionType(name -> new TranslatableText("cargument.criteria.invalid", name));
 
 	public static CScoreboardCriterionArgumentType scoreboardCriterion() {
@@ -54,17 +46,7 @@ public class CScoreboardCriterionArgumentType implements ArgumentType<Scoreboard
 
 	@Override
 	public <S> CompletableFuture<Suggestions> listSuggestions(final CommandContext<S> context, final SuggestionsBuilder builder) {
-		List<String> list = new ArrayList<>(ScoreboardCriterion.method_37271());
-
-		Registry.STAT_TYPE.stream().forEach(type -> type.forEach(value -> {
-			list.add(getName(Registry.STAT_TYPE.getId(type)) + ":" + value.getName());
-		}));
-
-		return CommandSource.suggestMatching(list, builder);
-	}
-
-	private static <T> String getName(Identifier id) {
-		return id.toString().replace(':', '.');
+		return CommandSource.suggestMatching(ScoreboardCriterion.method_37271(), builder);
 	}
 
 	@Override
