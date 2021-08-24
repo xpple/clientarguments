7f27a727f227ac61020291a24e17baf0de2bea9b
xpple <fred@xpple.dev>
2021-08-24 22:39:21 +0200

    Add command suggestions

--- src/main/java/dev/xpple/clientarguments/arguments/CEntitySummonArgumentType.java
+++ src/main/java/dev/xpple/clientarguments/arguments/CEntitySummonArgumentType.java
@@ -5,14 +5,19 @@ import com.mojang.brigadier.arguments.ArgumentType;
 import com.mojang.brigadier.context.CommandContext;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
 import com.mojang.brigadier.exceptions.DynamicCommandExceptionType;
-import net.minecraft.entity.EntityType;
+import com.mojang.brigadier.suggestion.Suggestions;
+import com.mojang.brigadier.suggestion.SuggestionsBuilder;
 import net.fabricmc.fabric.api.client.command.v1.FabricClientCommandSource;
+import net.minecraft.command.CommandSource;
+import net.minecraft.entity.EntityType;
 import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.concurrent.CompletableFuture;
+import java.util.stream.Stream;
 
 public class CEntitySummonArgumentType implements ArgumentType<Identifier> {
 
@@ -27,14 +32,22 @@ public class CEntitySummonArgumentType implements ArgumentType<Identifier> {
 		return validate(context.getArgument(name, Identifier.class));
 	}
 
+	private static Identifier validate(Identifier id) throws CommandSyntaxException {
+		Registry.ENTITY_TYPE.getOrEmpty(id).filter(EntityType::isSummonable).orElseThrow(() -> NOT_FOUND_EXCEPTION.create(id));
+		return id;
+	}
+
 	@Override
 	public Identifier parse(final StringReader stringReader) throws CommandSyntaxException {
 		return validate(Identifier.fromCommandInput(stringReader));
 	}
 
-	private static Identifier validate(Identifier id) throws CommandSyntaxException {
-		Registry.ENTITY_TYPE.getOrEmpty(id).filter(EntityType::isSummonable).orElseThrow(() -> NOT_FOUND_EXCEPTION.create(id));
-		return id;
+	@Override
+	public <S> CompletableFuture<Suggestions> listSuggestions(final CommandContext<S> context, final SuggestionsBuilder builder) {
+		Stream<Identifier> entities = Registry.ENTITY_TYPE.getIds().stream()
+				.filter(identifier -> Registry.ENTITY_TYPE.get(identifier).isSummonable());
+
+		return CommandSource.suggestIdentifiers(entities, builder);
 	}
 
 	@Override
