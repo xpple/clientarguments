aa8fea634c0fa22d82260c1c928f06de5abe5845
Fred <frederik.v.d.els@outlook.com>
2023-03-17 22:03:26 +0100

    Add missing argument types

---
+++ src/main/java/dev/xpple/clientarguments/arguments/CRegistryEntryArgumentType.java
@@ -0,0 +1,111 @@
+package dev.xpple.clientarguments.arguments;
+
+import com.mojang.brigadier.StringReader;
+import com.mojang.brigadier.arguments.ArgumentType;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.exceptions.Dynamic2CommandExceptionType;
+import com.mojang.brigadier.exceptions.Dynamic3CommandExceptionType;
+import com.mojang.brigadier.exceptions.DynamicCommandExceptionType;
+import com.mojang.brigadier.suggestion.Suggestions;
+import com.mojang.brigadier.suggestion.SuggestionsBuilder;
+import net.minecraft.command.CommandRegistryAccess;
+import net.minecraft.command.CommandSource;
+import net.minecraft.enchantment.Enchantment;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.attribute.EntityAttribute;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+import net.minecraft.world.gen.feature.ConfiguredFeature;
+import net.minecraft.world.gen.structure.Structure;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.concurrent.CompletableFuture;
+
+public class CRegistryEntryArgumentType<T> implements ArgumentType<RegistryEntry.Reference<T>> {
+
+    private static final Collection<String> EXAMPLES = Arrays.asList("foo", "foo:bar", "012");
+
+    private static final DynamicCommandExceptionType NOT_SUMMONABLE_EXCEPTION = new DynamicCommandExceptionType(id -> Text.translatable("entity.not_summonable", id));
+    public static final Dynamic2CommandExceptionType NOT_FOUND_EXCEPTION = new Dynamic2CommandExceptionType((element, type) -> Text.translatable("argument.resource.not_found", element, type));
+    public static final Dynamic3CommandExceptionType INVALID_TYPE_EXCEPTION = new Dynamic3CommandExceptionType((element, type, expectedType) -> Text.translatable("argument.resource.invalid_type", element, type, expectedType));
+
+    final RegistryKey<? extends Registry<T>> registryRef;
+    private final RegistryWrapper<T> registryWrapper;
+
+    public CRegistryEntryArgumentType(CommandRegistryAccess arg, RegistryKey<? extends Registry<T>> arg2) {
+        this.registryRef = arg2;
+        this.registryWrapper = arg.createWrapper(arg2);
+    }
+
+    public static <T> CRegistryEntryArgumentType<T> registryEntry(CommandRegistryAccess registryAccess, RegistryKey<? extends Registry<T>> registryRef) {
+        return new CRegistryEntryArgumentType<>(registryAccess, registryRef);
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <T> RegistryEntry.Reference<T> getCRegistryEntry(CommandContext<ServerCommandSource> context, String name, RegistryKey<Registry<T>> registryRef) throws CommandSyntaxException {
+        RegistryEntry.Reference<T> reference = (RegistryEntry.Reference<T>) context.getArgument(name, RegistryEntry.Reference.class);
+        RegistryKey<?> registryKey = reference.registryKey();
+        if (registryKey.isOf(registryRef)) {
+            return reference;
+        }
+        throw INVALID_TYPE_EXCEPTION.create(registryKey.getValue(), registryKey.getRegistry(), registryRef.getValue());
+    }
+
+    public static RegistryEntry.Reference<EntityAttribute> getCEntityAttribute(CommandContext<ServerCommandSource> context, String name) throws CommandSyntaxException {
+        return getCRegistryEntry(context, name, RegistryKeys.ATTRIBUTE);
+    }
+
+    public static RegistryEntry.Reference<ConfiguredFeature<?, ?>> getCConfiguredFeature(CommandContext<ServerCommandSource> context, String name) throws CommandSyntaxException {
+        return getCRegistryEntry(context, name, RegistryKeys.CONFIGURED_FEATURE);
+    }
+
+    public static RegistryEntry.Reference<Structure> getCStructure(CommandContext<ServerCommandSource> context, String name) throws CommandSyntaxException {
+        return getCRegistryEntry(context, name, RegistryKeys.STRUCTURE);
+    }
+
+    public static RegistryEntry.Reference<EntityType<?>> getCEntityType(CommandContext<ServerCommandSource> context, String name) throws CommandSyntaxException {
+        return getCRegistryEntry(context, name, RegistryKeys.ENTITY_TYPE);
+    }
+
+    public static RegistryEntry.Reference<EntityType<?>> getCSummonableEntityType(CommandContext<ServerCommandSource> context, String name) throws CommandSyntaxException {
+        RegistryEntry.Reference<EntityType<?>> lv = getCRegistryEntry(context, name, RegistryKeys.ENTITY_TYPE);
+        if (lv.value().isSummonable()) {
+            return lv;
+        }
+        throw NOT_SUMMONABLE_EXCEPTION.create(lv.registryKey().getValue().toString());
+    }
+
+    public static RegistryEntry.Reference<StatusEffect> getCStatusEffect(CommandContext<ServerCommandSource> context, String name) throws CommandSyntaxException {
+        return getCRegistryEntry(context, name, RegistryKeys.STATUS_EFFECT);
+    }
+
+    public static RegistryEntry.Reference<Enchantment> getCEnchantment(CommandContext<ServerCommandSource> context, String name) throws CommandSyntaxException {
+        return getCRegistryEntry(context, name, RegistryKeys.ENCHANTMENT);
+    }
+
+    @Override
+    public RegistryEntry.Reference<T> parse(final StringReader stringReader) throws CommandSyntaxException {
+        Identifier identifier = Identifier.fromCommandInput(stringReader);
+        RegistryKey<T> registryKey = RegistryKey.of(this.registryRef, identifier);
+        return this.registryWrapper.getOptional(registryKey).orElseThrow(() -> NOT_FOUND_EXCEPTION.create(identifier, this.registryRef.getValue()));
+    }
+
+    @Override
+    public <S> CompletableFuture<Suggestions> listSuggestions(final CommandContext<S> commandContext, final SuggestionsBuilder suggestionsBuilder) {
+        return CommandSource.suggestIdentifiers(this.registryWrapper.streamKeys().map(RegistryKey::getValue), suggestionsBuilder);
+    }
+
+    @Override
+    public Collection<String> getExamples() {
+        return EXAMPLES;
+    }
+}
---
+++ src/main/java/dev/xpple/clientarguments/arguments/CRegistryEntryPredicateArgumentType.java
@@ -0,0 +1,159 @@
+package dev.xpple.clientarguments.arguments;
+
+import com.mojang.brigadier.StringReader;
+import com.mojang.brigadier.arguments.ArgumentType;
+import com.mojang.brigadier.context.CommandContext;
+import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import com.mojang.brigadier.exceptions.Dynamic2CommandExceptionType;
+import com.mojang.brigadier.exceptions.Dynamic3CommandExceptionType;
+import com.mojang.brigadier.suggestion.Suggestions;
+import com.mojang.brigadier.suggestion.SuggestionsBuilder;
+import com.mojang.datafixers.util.Either;
+import net.minecraft.command.CommandRegistryAccess;
+import net.minecraft.command.CommandSource;
+import net.minecraft.command.argument.RegistryEntryArgumentType;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryWrapper;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.registry.entry.RegistryEntryList;
+import net.minecraft.registry.tag.TagKey;
+import net.minecraft.server.command.ServerCommandSource;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Optional;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Predicate;
+
+public class CRegistryEntryPredicateArgumentType<T> implements ArgumentType<CRegistryEntryPredicateArgumentType.EntryPredicate<T>> {
+
+    private static final Collection<String> EXAMPLES = Arrays.asList("foo", "foo:bar", "012", "#skeletons", "#minecraft:skeletons");
+
+    private static final Dynamic2CommandExceptionType NOT_FOUND_EXCEPTION = new Dynamic2CommandExceptionType((tag, type) -> Text.translatable("argument.resource_tag.not_found", tag, type));
+    private static final Dynamic3CommandExceptionType WRONG_TYPE_EXCEPTION = new Dynamic3CommandExceptionType((tag, type, expectedType) -> Text.translatable("argument.resource_tag.invalid_type", tag, type, expectedType));
+
+    private final RegistryWrapper<T> registryWrapper;
+    final RegistryKey<? extends Registry<T>> registryRef;
+
+    public CRegistryEntryPredicateArgumentType(CommandRegistryAccess registryAccess, RegistryKey<? extends Registry<T>> registryRef) {
+        this.registryRef = registryRef;
+        this.registryWrapper = registryAccess.createWrapper(registryRef);
+    }
+
+    public static <T> CRegistryEntryPredicateArgumentType<T> registryEntryPredicate(CommandRegistryAccess registryRef, RegistryKey<? extends Registry<T>> registryAccess) {
+        return new CRegistryEntryPredicateArgumentType<>(registryRef, registryAccess);
+    }
+
+    public static <T> CRegistryEntryPredicateArgumentType.EntryPredicate<T> getRegistryEntryPredicate(final CommandContext<ServerCommandSource> context, final String name, RegistryKey<Registry<T>> registryRef) throws CommandSyntaxException {
+        CRegistryEntryPredicateArgumentType.EntryPredicate<?> entryPredicate = context.getArgument(name, CRegistryEntryPredicateArgumentType.EntryPredicate.class);
+        Optional<CRegistryEntryPredicateArgumentType.EntryPredicate<T>> optional = entryPredicate.tryCast(registryRef);
+        return optional.orElseThrow(() -> entryPredicate.getEntry().map(entry -> {
+            RegistryKey<?> registryKey = entry.registryKey();
+            return RegistryEntryArgumentType.INVALID_TYPE_EXCEPTION.create(registryKey.getValue(), registryKey.getRegistry(), registryRef.getValue());
+        }, entryList -> {
+            TagKey<?> tagKey = entryList.getTag();
+            return WRONG_TYPE_EXCEPTION.create(tagKey.id(), tagKey.registry(), registryRef.getValue());
+        }));
+    }
+
+    @Override
+    public CRegistryEntryPredicateArgumentType.EntryPredicate<T> parse(final StringReader stringReader) throws CommandSyntaxException {
+        if (stringReader.canRead() && stringReader.peek() == '#') {
+            int cursor = stringReader.getCursor();
+
+            try {
+                stringReader.skip();
+                Identifier identifier = Identifier.fromCommandInput(stringReader);
+                TagKey<T> tagKey = TagKey.of(this.registryRef, identifier);
+                RegistryEntryList.Named<T> named = this.registryWrapper.getOptional(tagKey).orElseThrow(() -> NOT_FOUND_EXCEPTION.create(identifier, this.registryRef.getValue()));
+                return new CRegistryEntryPredicateArgumentType.TagBased<>(named);
+            } catch (CommandSyntaxException e) {
+                stringReader.setCursor(cursor);
+                throw e;
+            }
+        } else {
+            Identifier identifier = Identifier.fromCommandInput(stringReader);
+            RegistryKey<T> registryKey = RegistryKey.of(this.registryRef, identifier);
+            RegistryEntry.Reference<T> reference = this.registryWrapper.getOptional(registryKey).orElseThrow(() -> RegistryEntryArgumentType.NOT_FOUND_EXCEPTION.create(identifier, this.registryRef.getValue()));
+            return new CRegistryEntryPredicateArgumentType.EntryBased<>(reference);
+        }
+    }
+
+    @Override
+    public <S> CompletableFuture<Suggestions> listSuggestions(final CommandContext<S> context, final SuggestionsBuilder builder) {
+        CommandSource.suggestIdentifiers(this.registryWrapper.streamTagKeys().map(TagKey::id), builder, "#");
+        return CommandSource.suggestIdentifiers(this.registryWrapper.streamKeys().map(RegistryKey::getValue), builder);
+    }
+
+    @Override
+    public Collection<String> getExamples() {
+        return EXAMPLES;
+    }
+
+    public interface EntryPredicate<T> extends Predicate<RegistryEntry<T>> {
+        Either<RegistryEntry.Reference<T>, RegistryEntryList.Named<T>> getEntry();
+
+        <E> Optional<CRegistryEntryPredicateArgumentType.EntryPredicate<E>> tryCast(RegistryKey<? extends Registry<E>> registryRef);
+
+        String asString();
+    }
+
+    private record TagBased<T>(RegistryEntryList.Named<T> tag) implements CRegistryEntryPredicateArgumentType.EntryPredicate<T> {
+
+        @Override
+        public Either<RegistryEntry.Reference<T>, RegistryEntryList.Named<T>> getEntry() {
+            return Either.right(this.tag);
+        }
+
+        @Override
+        @SuppressWarnings("unchecked")
+        public <E> Optional<CRegistryEntryPredicateArgumentType.EntryPredicate<E>> tryCast(RegistryKey<? extends Registry<E>> registryRef) {
+            return this.tag.getTag().isOf(registryRef) ? Optional.of((EntryPredicate<E>) this) : Optional.empty();
+        }
+
+        @Override
+        public boolean test(RegistryEntry<T> registryEntry) {
+            return this.tag.contains(registryEntry);
+        }
+
+        @Override
+        public String asString() {
+            return "#" + this.tag.getTag().id();
+        }
+
+        public RegistryEntryList.Named<T> tag() {
+            return this.tag;
+        }
+    }
+
+    private record EntryBased<T>(RegistryEntry.Reference<T> value) implements CRegistryEntryPredicateArgumentType.EntryPredicate<T> {
+
+        @Override
+        public Either<RegistryEntry.Reference<T>, RegistryEntryList.Named<T>> getEntry() {
+            return Either.left(this.value);
+        }
+
+        @Override
+        @SuppressWarnings("unchecked")
+        public <E> Optional<CRegistryEntryPredicateArgumentType.EntryPredicate<E>> tryCast(RegistryKey<? extends Registry<E>> registryRef) {
+            return this.value.registryKey().isOf(registryRef) ? Optional.of((EntryPredicate<E>) this) : Optional.empty();
+        }
+
+        @Override
+        public boolean test(RegistryEntry<T> registryEntry) {
+            return registryEntry.equals(this.value);
+        }
+
+        @Override
+        public String asString() {
+            return this.value.registryKey().getValue().toString();
+        }
+
+        public RegistryEntry.Reference<T> value() {
+            return this.value;
+        }
+    }
+}
